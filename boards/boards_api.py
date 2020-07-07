a029d994c0a3bc29d336cf4fcc204c57307de29e
Golden Snitch <goldensnitchtech@gmail.com>
2020-07-07 15:51:22 +0530

    Timesheet API with TokenAuth

--- boards/boards_api.py
+++ boards/boards_api.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
-from .models import Associate
+from .models import Associate, Timesheet
 from rest_framework import viewsets
 
 from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
+
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 
 User = get_user_model()
 
@@ -11,12 +15,29 @@ User = get_user_model()
 class AssociateSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = Associate
-		fields = ['First_Name','Last_Name','Date_of_Birth']
+		fields = ['id','First_Name','Last_Name','Date_of_Birth']
 
 
 class AssociateViewSet(viewsets.ModelViewSet):
 	queryset = Associate.objects.all()
 	serializer_class = AssociateSerializer
+
+
+
+
+
+class TimesheetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Timesheet
+        fields = ['id', 'Associate','Date','Details', 'Hours']
+
+
+class TimesheetViewSet(viewsets.ModelViewSet):
+    queryset = Timesheet.objects.all().order_by('Associate', '-Date')
+    serializer_class = TimesheetSerializer
+    authentication_classes = (TokenAuthentication, ) 
+    permission_classes = (IsAuthenticated, )
+
 
 
 
